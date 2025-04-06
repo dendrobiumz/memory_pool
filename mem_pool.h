1414d4ab066ef4702efc8d3d97c46126e33e2905
dendrobiumz <page19920610@gmail.com>
2025-04-06 14:12:25 +0800

    [Update] re-define struct mem_pool

--- mem_pool.h
+++ mem_pool.h
@@ -1,24 +1,19 @@
 #ifndef _MEM_POOL_H_
 #define _MEM_POOL_H_
 
-//#include "lfq.h"
 #include <stdatomic.h>
 #include <stdint.h>
 
 
-
-struct node {
-    struct node *next;
-};
-
 typedef struct mem_pool{
-    size_t blk_sz;
-    size_t blk_nbr;
-    atomic_int first_idx;
-    struct node list_head;
-    void *mem;
-    void* (*alloc_mem)(size_t num);
-    int   (*free_mem)(void* addr);    
+    int blk_sz;
+    int blk_nbrs;
+    atomic_int first_free_idx;
+    atomic_int init_blk_nbrs;
+    uintptr_t mem_start;
+    uintptr_t mem_end;
+    void* (*alloc)(struct mem_pool *self);
+    int   (*free)(struct mem_pool *self, void* addr);    
 } mem_pool_t;
 
 mem_pool_t* new_mem_pool(int obj_sz, int obj_nbrs);
